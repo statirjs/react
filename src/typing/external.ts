6000882f608caa8b32f572e820819d0a7491caef
bre30kra69cs <an.krasowsckij@gmail.com>
2020-06-14 00:38:38 +0300

    [fix] external types

--- src/typing/external.ts
+++ src/typing/external.ts
@@ -1,10 +1,10 @@
-import { Context as ReactContext, ReactNode } from 'react';
+import { Context as ReactContext } from 'react';
 import * as S from './internal';
 
 export declare function useSelect<T>(selector: S.StateSelector<T>): T;
 
 export declare function useDispatch<T>(selector: S.DispatchSelector<T>): T;
 
-export declare function Provider(props: S.ProviderProps): ReactNode;
+export declare function Provider(props: S.ProviderProps): JSX.Element;
 
 export declare const Context: ReactContext<S.ContextValue>;
